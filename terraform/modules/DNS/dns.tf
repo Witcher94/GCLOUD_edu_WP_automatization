a18f0ed95797d276302bc75a1286035ae89db275
witcher94 <pavlo.fakas@gmail.com>
2021-05-20 16:23:01 +0300

    change nds name

--- terraform/modules/DNS/dns.tf
+++ terraform/modules/DNS/dns.tf
@@ -6,7 +6,7 @@ resource "google_dns_managed_zone" "pfaka-pp" {
 
 resource "google_dns_record_set" "pfaka-pp" {
   managed_zone = google_dns_managed_zone.pfaka-pp.name
-  name         = "www.pfaka.pp.ua."
+  name         = "pfaka.pp.ua."
   type         = "A"
   rrdatas      = var.wp-address
   ttl          = 300
